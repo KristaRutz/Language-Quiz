535a9f3c67931daccede466b56d0f5b72edf1edb
Krista Rutz <rutzkri000@gmail.com>
2020-01-17 11:02:27 -0800

    add coding style question

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   $("form").submit(function(event) {
     event.preventDefault();
+
+    var username = $("#username").val();
+    console.log(username);
+    
+    console.log($("input:radio[name=hogwarts]:checked").val());
+    console.log($("input:radio[name=describesYou]:checked").val());
+
+
+    $("#resultMessage").show();
+
   })
 });
